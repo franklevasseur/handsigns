fd3eee8747c23f7cf01afb21e367d2e3c00538d8
François Levasseur <francois_levasseur@hotmail.com>
2022-09-16 17:09:00 -0400

    display hand placement

--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,43 @@
-import rich
+import cv2
+import mediapipe as mp
 
-rich.print("Hello, world!")
+cap = cv2.VideoCapture(0)
+mp_hands = mp.solutions.hands  # type: ignore
+hands = mp_hands.Hands()
+mp_draw = mp.solutions.drawing_utils  # type: ignore
+
+
+# Check if the webcam is opened correctly
+if not cap.isOpened():
+    raise IOError("Cannot open webcam")
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            index = hand_landmarks.landmark[8]
+            thumb = hand_landmarks.landmark[4]
+            cv2.putText(img, f"x={index.x:,.2f} y={index.y:,.2f} z={index.z:,.2f}", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            cv2.putText(img, f"x={thumb.x:,.2f} y={thumb.y:,.2f} z={thumb.z:,.2f}", (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+
+            for id, lm in enumerate(hand_landmarks.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                if id == 8:
+                    cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+                elif id == 4:
+                    cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+                else:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+
+    cv2.imshow('Input', img)
+
+    c = cv2.waitKey(1)
+    if c == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
